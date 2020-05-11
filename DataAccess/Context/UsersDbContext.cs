961689b110ac4c671826b70ec0cdc17265f63941
Manuel Villanueva <1mm.villanueva1@gmail.com>
2020-05-10 23:10:14 -0600

    estructura inicial del proyecto, model, context, interfaces y clases de servicio a utilizar

---
+++ DataAccess/Context/UsersDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Context
+{
+    public class UsersDbContext: DbContext
+    {
+        public UsersDbContext(DbContextOptions<UsersDbContext> options): base(options)
+        {
+
+        }
+
+       public DbSet<Users> Users { get; set; }
+    }
+}
---
+++ DataAccess/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Text;
+
+namespace DataAccess.Models
+{
+    public class Users
+    {
+        [Key]
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
