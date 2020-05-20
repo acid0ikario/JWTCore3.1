d077317932fe02bdc06c7cbb07959291f0216763
Manuel Villanueva <1mm.villanueva1@gmail.com>
2020-05-20 16:42:42 -0600

    capa de servicios para autenticar

--- BusinessServices/Services/AuthenticationService.cs
+++ BusinessServices/Services/AuthenticationService.cs
@@ -1,10 +1,42 @@
-﻿using System;
+﻿using BusinessServices.Interfaces;
+using DataAccess.Context;
+using DataAccess.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace BusinessServices.Services
 {
-   public class AuthenticationService
+   public class AuthenticationService: IAuthenticationService
     {
+        private readonly string _secret;
+        public AuthenticationService(IConfiguration config)
+        {
+            _secret = config.GetValue<string>("DevSecretKey");
+        }
+
+        public string GenerateSecurityToken(Users usuario)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, usuario.UserId),
+                    new Claim(ClaimTypes.Role, usuario.RolId)
+                }),
+                Expires = DateTime.UtcNow.AddDays(30),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
     }
 }
--- BusinessServices/Services/UsersService.cs
+++ BusinessServices/Services/UsersService.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using BusinessServices.Interfaces;
+using DataAccess.Context;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Linq;
+using DataAccess.Models;
 
 namespace BusinessServices.Services
 {
-   public class UsersService
+    public class UsersService : IUsersService
     {
+        private readonly UsersDbContext _dbContext;
+       
+        public UsersService(UsersDbContext context, IUsersService usersService)
+        {
+            _dbContext = context;
+           
+        }
+        public Users AuthUser(string user, string password)
+        {
+            var AuthUser = _dbContext.Users.First(x => x.UserId == user && x.Password == password);
+            if (user == null)
+                throw new Exception("usuario o contraseña invalidos");
+
+            return AuthUser;
+        }
     }
 }
